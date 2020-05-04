ba55f59bf6110f18a2ef5b3278d16d85bb796ba5
Ivana <ivana_pile@yahoo.com>
2020-05-03 21:20:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+// map returns an array with all values made negative
+function map(sourceArray, callback) {
+    let r = []
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        let theElement = sourceArray[i]
+        r.push(callback(theElement))
+    }
+    return r;
+}
+
+function reduce( sourceArray, callback, starting) {
+    let r = (!!starting) ? starting : sourceArray[0]
+    let i = (!!starting) ? 0:1
+
+    for (; i < sourceArray.length; i++) {
+        r = callback(sourceArray[i], r)
+    }
+
+    return r;
+}
